ee58ca363faad5fc3f7233064a56e6962739323d
JOS-RE <joshithreddy.g@gmail.com>
2021-09-15 21:21:10 +0530

    added distance function and normal mapping ui

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import math
 import cv2
 import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
@@ -30,22 +31,51 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
-    mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
+    # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
 
     
 
     cv2.imshow('MediaPipe Holistic', image)
-    if cv2.waitKey(10) & 0xFF == ord('q'):
+    if cv2.waitKey(5) & 0xFF == ord('q'):
       break
     
-# for data in results.pose_landmarks.landmark:
-#   keypoints.append({'X': data.x,
-#                     'Y': data.y,
-#                     'Z': data.z,
-#                     # 'Visibility': data.visibility,
-#                     })
-# print(keypoints)
+for data in results.pose_landmarks.landmark:
+  keypoints.append({'X': data.x,
+                    'Y': data.y,
+                    'Z': data.z,
+                    
+                    # 'Visibility': data.visibility,
+                    })
+
+for i in range(len(keypoints)):
+  print(f'{i}\n {keypoints[i]}\n\n')
+
+a = keypoints[9]  # soulder arc
+b = keypoints[22] # right arc
+c = keypoints[23] # left arc
+d = keypoints[24] # hip arc
+
+
 # print(results.pose_landmarks)
+
+def calculate(C1, C2):
+        dist = 0
+        dist += ((C1['X']) - (C2['X']))**2
+        dist += ((C1['Y']) - (C2['Y']))**2
+        dist += ((C1['Z']) - (C2['Z']))**2
+        result = math.sqrt(dist)
+        print(result)
+
+print(f'distance : {calculate(a,d)} ')
 
 cap.release()
 
+
+#  for - i (0 -> 10)
+#   each : 5 sec me "Q" 
+# 
+# 
+# 
+# 
+# 
+#  
