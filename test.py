e5ef3096f9021c6c849218824a6fd966acdcfd98
G.Hemanth Sai <hemanth.sai3187@gmail.com>
2021-09-16 18:16:48 +0530

    voice is working

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-import azure.cognitiveservices.speech as speech_sdk
-import os
+import pyttsx3
 from dotenv import load_dotenv
 import math
 import cv2
@@ -8,7 +7,7 @@ import mediapipe as mp
 # Important functions to be used in pose detection
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-mp_holistic = mp.solutions.holistic
+mp_pose = mp.solutions.pose
 
 
 # def CalculateTime(t):
@@ -26,40 +25,37 @@ mp_holistic = mp.solutions.holistic
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
-        success, image = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
-            continue
+with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5) as pose:
+  while cap.isOpened():
+    success, image = cap.read()
+    if not success:
+      print("Ignoring empty camera frame.")
+      # If loading a video, use 'break' instead of 'continue'.
+      continue
 
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
-        image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # Flip the image horizontally for a later selfie-view display, and convert
+    # the BGR image to RGB.
+    image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
+    # To improve performance, optionally mark the image as not writeable to
+    # pass by reference.
+    image.flags.writeable = False
+    results = pose.process(image)
 
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-        image.flags.writeable = False
-        results = holistic.process(image)
+    # Draw the pose annotation on the image.
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    mp_drawing.draw_landmarks(image,results.pose_landmarks,mp_pose.POSE_CONNECTIONS,landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
-        # Draw landmark annotation on the image.
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
 
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                                  landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+    keypoints = [{'X': data.x, 'Y': data.y, 'Z': data.z} for data in results.pose_landmarks.landmark]
 
-        # mp_drawing.plot_landmarks(results.pose_world_landmarks, mp_holistic.POSE_CONNECTIONS)
-
-        keypoints = [{'X': data.x, 'Y': data.y, 'Z': data.z}
-                     for data in results.pose_landmarks.landmark]
-
-        cv2.imshow('MediaPipe Holistic', image)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+    cv2.imshow('MediaPipe Pose', image)
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+      break
 cap.release()
-
 # To calculate the distance
 
 for i in range(len(keypoints)):
@@ -76,53 +72,12 @@ def calculateDistance(C1, C2):
     dist += ((C1['Y']) - (C2['Y']))**2
     dist += ((C1['Z']) - (C2['Z']))**2
     result = math.sqrt(dist)
-    print(result)
+    return result
+distance = calculateDistance(arc1, arc4)
 
-# Azure text to speech
-
-
-def main():
-    try:
-        global speech_config
-        global translation_config
-
-        # Get Configuration Setttings
-        load_dotenv()
-        cog_key = os.getenv('COG_SERVICE_KEY')
-        cog_region = os.getenv('COG_sERVICE_REGION')
-
-        # Configure speech service
-        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
-        print('Ready to use speech service in:', speech_config.region)
-
-    except Exception as ex:
-        print(ex)
+if distance < 1:
+    engine = pyttsx3.init()
+    engine.say('Sit straight you moron')
+    engine.runAndWait()
 
 
-def TellWarning():
-    if calculateDistance(arc1, arc4) < 0.7*0.9665996894753616:
-        response_text = 'Sit straight you moron'
-
-    # Configure speech synthesis
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
-    speech_config.speech_synthesis_voice_name = 'en-GB-george'
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
-
-    # Synthesize spoken output
-    responseSsml = f"\
-        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
-            <voice name='en-GB-Susan'> \
-                {response_text} \
-                    <break strength='weak'/> \
-                        </voice> \
-        </speak>"
-    speak = speech_synthesizer.speak_ssml_async(response_text).get()
-    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
-        print(speak.reason)
-
-    # Print the response
-    print(response_text)
-
-
-if __name__ == '__main__':
-    main()
--- tts.py
+++ tts.py
@@ -1,48 +1,83 @@
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 import azure.cognitiveservices.speech as speech_sdk
+
+# Import namespaces
+
 
 def main():
     try:
         global speech_config
-        global translation_config
 
-        #Get Configuration Setttings
+        # Get Configuration Settings
         load_dotenv()
-        cog_key=os.getenv('COG_SERVICE_KEY')
-        cog_region=os.getenv('COG_sERVICE_REGION')
+        cog_key = os.getenv('COG_SERVICE_KEY')
+        cog_region = os.getenv('COG_SERVICE_REGION')
 
-        #Configure speech service
-        speech_config=speech_sdk.SpeechConfig(cog_key,cog_region)
-        print('Ready to use speech service in:',speech_config.region)
+        # Configure speech service
+        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
+        print("Ready to use speech service in:", speech_config.region)
+
+        # Get spoken input
+        command = TranscribeCommand()
+        if command.lower() == 'what time is it?' or "what's the time?":
+            TellTime()
 
     except Exception as ex:
-        print(ex)    
+        print(ex)
 
-def TellWarning():
-    response_text=f'Sit straight you moron'
 
-    #Configure speech synthesis
-    speech_synthesizer=speech_sdk.SpeechSynthesizer(speech_config)
-    speech_config.speech_synthesis_voice_name='en-GB-george'
-    speech_synthesizer=speech_sdk.SpeechSynthesizer(speech_config)
+def TranscribeCommand():
+    command = ''
 
-    #Synthesize spoken output
-    responseSsml = f"\
+    # Configure speech recognition
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(
+        speech_config, audio_config)
+    print('Speak now...')
+
+    # Process speech input
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
+    # Return the command
+    return command
+
+
+def TellTime():
+    now = datetime.now()
+    response_text = 'The time is {}:{:02d}'.format(now.hour, now.minute)
+
+    # Configure speech synthesis
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+    speech_config.speech_synthesis_voice_name = 'en-GB-george'
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+
+    # Synthesize spoken output
+    responseSsml = "\
         <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
             <voice name='en-GB-Susan'> \
-                {response_text} \
+                {} \
                     <break strength='weak'/> \
                         </voice> \
-        </speak>"
-    speak=speech_synthesizer.speak_ssml_async(response_text).get()
-    if speak.reason!=speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
         print(speak.reason)
 
-    #Print the response
+    # Print the response
     print(response_text)
 
-if __name__=='__main__':
-    main()        
+
+if __name__ == "__main__":
+    main()
 
 
